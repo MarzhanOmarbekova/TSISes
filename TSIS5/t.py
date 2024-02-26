4023b16773bd5859ab9a5278356728cfef21aacc
marginonlytop <mark.omarbek@gmail.com>
2024-02-26 22:27:06 +0600

    tsis5

---
+++ TSIS5/t.py
@@ -0,0 +1,19 @@
+# meta characters
+
+# * - 0 or more occurences
+# + - 1 or more occurences
+# . - any character (except a new line)
+# | - either or
+# ^ - matches only at the beginning of the string
+# $ - matches only at the end of the string
+
+
+# Quantifiers
+
+# * - 0 or more occurences (or {0,})
+# + - 1 or more occurences (or {1,})
+# ? - 0 or 1 occurence (or {0,1})
+# {2} - exactly 2 occurences
+# {1,3} - from 1 occurence to 3 occurences
+# {1,} - at least 1 occurence (same as +)
+# {,3} - no more than 3 occurences
